90f2fd36b4178a83e5e573a6f5e6d8a8423774c5
vml-akoebbe <akoebbe@vml.com>
2013-05-21 22:41:09 -0700

    Base theme cleanup added compass-normalize

--- sites/all/themes/drupal4ok/stylesheets/config.rb
+++ sites/all/themes/drupal4ok/stylesheets/config.rb
@@ -1,11 +1,12 @@
 # Require any additional compass plugins here.
+require 'compass-normalize'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "../images"
-javascripts_dir = "../javascripts"
+javascripts_dir = "../scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -14,7 +15,7 @@ javascripts_dir = "../javascripts"
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
